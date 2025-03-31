63e99b6aa314c0e774444f75e3775d18f13bd400
elin-sunhye <tjsgp43@gmail.com>
2025-03-31 13:35:45 +0900

    lib 설치, layout, component 설정

---
+++ component/common/Input/Input.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import {
+  ChangeEventHandler,
+  JSX,
+  KeyboardEventHandler,
+  Ref,
+  forwardRef,
+} from "react";
+import style from "./input.module.scss";
+interface InputProps {
+  size?: "sm" | "md" | "xlg";
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+  onKeyUp?: KeyboardEventHandler<HTMLInputElement>;
+  // 웹접근성
+  id: string;
+  labelNm: string;
+  type: "text" | "password";
+  value?: string;
+  name: string;
+}
+
+/**
+ * @size ?: 버튼의 사이즈 기본 lg
+ *
+ * @onChange ?:
+ *
+ * * 접근성
+ *
+ * @id : id
+ *
+ * @labelNm :  인풋 이름 라벨
+ *
+ * @type : 인풋 타입
+ *
+ * @value : value
+ *
+ * @name : name
+ */
+const Input = forwardRef(
+  (
+    {
+      size,
+      id,
+      labelNm,
+      type,
+      value,
+      name,
+      onChange,
+      onKeyUp,
+      ...props
+    }: InputProps & React.InputHTMLAttributes<HTMLInputElement>,
+    ref: Ref<HTMLInputElement>
+  ) => {
+    return (
+      <>
+        <label htmlFor={id} className="screen_out">
+          {labelNm}
+        </label>
+        <input
+          ref={ref}
+          type={type}
+          name={name}
+          id={id}
+          className={`${style.input} ${size ? style[size] : style.lg}`}
+          value={value}
+          onChange={onChange}
+          onKeyUp={onKeyUp}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault();
+            }
+          }}
+          autoComplete={"off"}
+          {...props}
+        />
+      </>
+    );
+  }
+);
+
+Input.displayName = "Input";
+export default Input as <T extends {}>(
+  props: InputProps & {
+    ref: Ref<HTMLInputElement>;
+  }
+) => JSX.Element;
---
+++ types/nextAuth/next-auth.d.ts
@@ -0,0 +1,65 @@
+import NextAuth from "next-auth";
+import { EnterpriseInfoResponse } from "../lg/RegisterType";
+
+interface AuthListType {
+  authrtSeq: number;
+  authrtCd: string;
+  authrtNm: string;
+  authrtExpln: string;
+}
+
+declare module "next-auth" {
+  interface User {
+    /**
+     * @userSeq: 사용자 시퀀스
+     */
+    userSeq: number;
+    /**
+     * @userNm: 성명
+     */
+    userNm: string;
+    /**
+     * @userBirth: 생년월일
+     */
+    userBirth: string | null;
+    /**
+     * @userTelno: 연락처
+     */
+    userTelno: string;
+    /**
+     * @userId: 아이디
+     */
+    userId: string;
+    /**
+     * @userEmail: 이메일
+     */
+    userEmail: string;
+    /**
+     * @authList: 권한리스트
+     */
+    auth: string;
+    /**
+     * @jwtAuthToken: Auth 토큰
+     */
+    jwtAuthToken?: string;
+    /**
+     * @jwtRefreshToken: Refresh 토큰
+     */
+    jwtRefreshToken?: string;
+
+    // 오류메시지: 오류일때만 내용이 담겨서 옴
+    code?: string;
+    description?: string;
+    detail?: string;
+  }
+
+  interface Session {
+    user: User;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    user: User;
+  }
+}
